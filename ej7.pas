abd38f41b8ada6a7f569dfda2026c6273225b822
Bruno <25409519+brunoeliam@users.noreply.github.com>
2023-10-14 12:34:08 -0300

    Add files via upload

--- ej7.pas
+++ ej7.pas
@@ -1,34 +1,89 @@
-(* a. Realizar un módulo que reciba como parámetro un número entero y retorne la cantidad de digitos que posee y la suma de los mismos.
-b. Utilizando el modulo anterior, realizar un programa que lea una secuencia de números e imprima la cantidad total de digitos leídos. La lectura finaliza al leer un número cuyos dígitos suman exactamente 10, el cual debe procesarse.
-*)
+(* 7. Realizar un programa que lea información de centros de investigación de Universidades Nacionales. De cada centro se lee su nombre abreviado (ej. LIDI, LIFIA, LINTI), la universidad a la que pertenece, la cantidad de investigadores y la cantidad de becarios que poseen. La información se lee de forma consecutiva por universidad y la lectura finaliza al leer un centro con 0 investigadores, que no debe procesarse. Informar:
+- Cantidad total de centros para cada universidad.
+- Universidad con mayor cantidad de investigadores en sus centros.
+- Los dos centros con menor cantidad de becarios. *)
 
 program ej7;
-procedure sumador(num: integer; var digitos, suma: integer);
-var
-  ultimoDigito, resto: integer;
-  begin 
-    suma:= 0;
-    digitos:= 0;
-    while (num <> 0) do 
-      begin
-        digitos:= digitos + 1;
-        ultimoDigito:= num mod 10;
-        suma:= suma + ultimoDigito;
-        num:= num div 10;
-      end;
+type
+  centro = record
+  nombre: string;
+  universidad: string;
+  cantInv: integer;
+  cantBec: integer;
   end;
 
+  minCentro = record
+  nombre: string;
+  cantBec: integer;
+  end;
+
+  maxCentro = record
+  universidad: string;
+  cantInv: integer;
+  end;  
+
+procedure leer(var c: centro);
+  begin
+    writeln('nombre: '); readln(c.nombre);
+    writeln('universidad: '); readln(c.universidad);
+    writeln('cantidad de becarios: '); readln(c.cantBec);
+  end;
+
+procedure calcularMin(c: centro; var min1, min2: minCentro);
+begin
+    if (c.cantBec < min1.cantBec) then
+      begin
+        min2.cantBec:= min1.cantBec;
+        min2.nombre:= min1.nombre;
+        min1.cantBec:= c.cantBec;
+        min1.nombre:= c.nombre;
+      end
+    else if (c.cantBec < min2.cantBec) then
+      begin
+        min2.cantBec:= c.cantBec;
+        min2.nombre:= c.nombre;
+      end;
+end;
+
+procedure calcularMax(c: centro; var max: maxCentro);
+begin
+  if (c.cantInv > max.cantInv) then
+    begin
+      max.cantInv:= c.cantInv;
+      max.universidad:= c.universidad;
+    end;
+end;
+
 var
-num, digitos, suma, totalDigitos: integer;
+c: centro;
+max: maxCentro;
+univActual: string;
+cantCentros: integer;
+min1, min2: minCentro;
+
 begin 
-  suma:= 0;
-  digitos:= 0;
-  totalDigitos:= 0;
-  repeat
-    WriteLn('ingrese un numero ');
-    ReadLn(num); 
-    sumador(num, digitos, suma);
-    totalDigitos:= totalDigitos + digitos;
-  until (suma = 10);
-  WriteLn('el total de digitos leidos fue de ' ,totalDigitos);
+  max.universidad:= '';
+  max.cantInv:= -1;
+  min1.nombre:= '';
+  min2.nombre:= '';
+  min1.cantBec:= 9997;
+  min2.cantBec:= 9999;
+
+  writeln('cantidad de investigadores: '); readln(c.cantInv);
+  while (c.cantInv <> 0) do
+  begin
+    leer(c);
+    cantCentros:= 0;
+    univActual:= c.universidad;
+    while (c.cantInv <> 0) and (univActual = c.universidad) do
+      begin
+        cantCentros:= cantCentros + 1;
+        calcularMin(c,min1, min2);
+        calcularMax(c,max);
+        writeln('cantidad de investigadores: '); readln(c.cantInv);
+        leer(c);
+      end;
+    writeln('la cantidad de centros de la universidad ', univActual ,' fue de ' , cantCentros);
+  end;
+  writeln('la universidad con mayor cantidad de investigadores en sus centros fue ' , max.universidad , ' y los dos centros con menor cantidad de becarios fueron los de ', min1.nombre , min2.nombre);
 end.
